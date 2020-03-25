ecaebb4b5e4f74b424ca4bdafe7f8dfc85b58d9c
Giorgi Zhorzholiani <jorjika90@gmail.com>
2020-03-25 11:42:36 +0400

    First upload

---
+++ src/JorJika.S3/IS3Client.cs
@@ -0,0 +1,99 @@
+﻿using JorJika.S3.Models;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace JorJika.S3
+{
+    /// <summary>
+    /// S3 client interface
+    /// </summary>
+    public interface S3Client
+    {
+        #region Bucket Operations
+
+        /// <summary>
+        /// Create bucket
+        /// </summary>
+        /// <param name="bucketName">Bucket name - Validation: lower case alpha numeric characters plus dots.</param>
+        /// <returns></returns>
+        Task CreateBucket(string bucketName);
+
+        /// <summary>
+        /// Remove bucket
+        /// </summary>
+        /// <param name="bucketName"></param>
+        /// <returns></returns>
+        Task RemoveBucket(string bucketName);
+
+        #endregion
+
+        #region Object Operations
+
+        /// <summary>
+        /// Gets object information - Without file data
+        /// </summary>
+        /// <param name="objectName">Object name</param>
+        /// <param name="bucketName">Bucket name - Optional if passed throuhg constructor</param>
+        /// <returns>Returns object information and metadata</returns>
+        Task<S3Object> GetObjectInfo(string objectName, string bucketName = null);
+
+        /// <summary>
+        /// Get object - With data
+        /// </summary>
+        /// <param name="objectName">Object name</param>
+        /// <param name="bucketName">Bucket name - Optional if passed throuhg constructor</param>
+        /// <returns>Returns actual object data - bytes</returns>
+        Task<S3Object> GetObject(string objectName, string bucketName = null);
+
+        /// <summary>
+        /// Gets object URL for downloading from storage (Temporary URL support varies by implementation)
+        /// </summary>
+        /// <param name="objectName">Object name</param>
+        /// <param name="bucketName">Bucket name - Optional if passed throuhg constructor</param>
+        /// <returns>Returns temporary URL of object for download</returns>
+        Task<string> GetObjectURL(string objectName, string bucketName = null);
+
+        /// <summary>
+        /// Save binary object
+        /// </summary>
+        /// <param name="objectName">Object name</param>
+        /// <param name="objectData">Object byte array</param>
+        /// <param name="contentType">Content type - Optional (Used for PDF and text files to directly show in browser when issuing temporary link)</param>
+        /// <param name="metaData">Object meta data</param>
+        /// <param name="bucketName">Bucket name - Optional if passed throuhg constructor</param>
+        /// <returns></returns>
+        Task SaveObject(string objectName, byte[] objectData, string contentType = null, Dictionary<string, string> metaData = null, string bucketName = null);
+
+        /// <summary>
+        /// Removes object from storage
+        /// </summary>
+        /// <param name="objectName">Object name</param>
+        /// <param name="bucketName">Bucket name - Optional if passed throuhg constructor</param>
+        /// <returns></returns>
+        Task RemoveObject(string objectName, string bucketName = null);
+
+        /// <summary>
+        /// Save text file to storage
+        /// </summary>
+        /// <param name="objectName">Object name</param>
+        /// <param name="content">Text file content</param>
+        /// <param name="fileName">File name - Optional (If you are downloading file from browser file name is automatically filled with this value)</param>
+        /// <param name="fileExtension">File extension, defaults to txt.</param>
+        /// <param name="bucketName">Bucket name - Optional if passed throuhg constructor</param>
+        /// <returns></returns>
+        Task SaveText(string objectName, string content, string fileName = null, string fileExtension = "txt", string bucketName = null);
+
+        /// <summary>
+        /// Save pdf file to storage
+        /// </summary>
+        /// <param name="objectName">Object name</param>
+        /// <param name="objectData">PDF file byte array</param>
+        /// <param name="fileName">File name - Optional (If you are downloading file from browser file name is automatically filled with this value)</param>
+        /// <param name="bucketName">Bucket name - Optional if passed throuhg constructor</param>
+        /// <returns></returns>
+        Task SavePDF(string objectName, byte[] objectData, string fileName = null, string bucketName = null);
+
+        #endregion
+    }
+}
---
+++ src/JorJika.S3/Models/S3Object.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace JorJika.S3.Models
+{
+    public class S3Object
+    {
+        public S3Object(string objectName,
+           string bucketName,
+           long size,
+           string eTag,
+           string contentType,
+           Dictionary<string, string> metaData,
+           byte[] data)
+        {
+            ObjectName = objectName;
+            BucketName = bucketName;
+            Size = size;
+            ETag = eTag;
+            ContentType = contentType;
+            MetaData = metaData;
+            Data = data;
+        }
+
+        public string ObjectName { get; set; }
+        public string BucketName { get; set; }
+        public long Size { get; set; }
+        public string ETag { get; set; }
+        public string ContentType { get; set; }
+        public Dictionary<string, string> MetaData { get; set; }
+
+        public byte[] Data { get; set; }
+
+        public string DataAsString() => Data == null ? null : Encoding.UTF8.GetString(Data);
+
+        public override string ToString()
+        {
+            return $"{BucketName}/{ObjectName}";
+        }
+    }
+}
