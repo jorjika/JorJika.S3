725dc0aabfc527c428ba6991353cedcae1d3ccf0
Giorgi Zhorzholiani <jorjika90@gmail.com>
2020-05-03 13:53:21 +0400

    object name tests added

--- src/JorJika.S3.Minio.Tests/ValidationTests.cs
+++ src/JorJika.S3.Minio.Tests/ValidationTests.cs
@@ -11,9 +11,37 @@ namespace JorJika.S3.Minio.Tests
     public class ValidationTests
     {
         [Fact]
-        public void Object_Name_Should_Be_Valid()
+        public void Object_name_with_score_should_be_valid()
         {
-            Action act = () => Validation.ValidateObjectName("object-image-name-510bfb20-b3cf-4fdb-bf02-f00e18640086_123.jpg");
+            Action act = () => Validation.ValidateObjectName("object-name");
+            act.Should().NotThrow();
+        }
+
+        [Fact]
+        public void Object_name_with_underscore_should_be_valid()
+        {
+            Action act = () => Validation.ValidateObjectName("object_name");
+            act.Should().NotThrow();
+        }
+
+        [Fact]
+        public void Object_name_with_dot_should_be_valid()
+        {
+            Action act = () => Validation.ValidateObjectName("object.name");
+            act.Should().NotThrow();
+        }
+
+        [Fact]
+        public void Object_name_with_slash_should_be_valid()
+        {
+            Action act = () => Validation.ValidateObjectName("object/name");
+            act.Should().NotThrow();
+        }
+        
+        [Fact]
+        public void Object_name_with_numbers_should_be_valid()
+        {
+            Action act = () => Validation.ValidateObjectName("objectname123");
             act.Should().NotThrow();
         }
 
